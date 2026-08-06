94133359d12c42d0a94b113b31ae78d1edc19a86
Bobo635-hub <wanjohirachel7@gmail.com>
2026-08-06 03:00:31 -0700

    add movie posters on home page

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const movieList = document.getElementById("movie-list");
 
-const movies = JSON.parse(localStorage.getItem("movies")) || [];
+const movies = [
+  {
+    title: "Minions and Monsters",
+    genre: "Comedy",
+    showtime: "8:00 PM",
+    poster: "https://m.media-amazon.com/images/M/MV5BZjZkNzgyYTUtNjA1Yy00OTNmLTg5YWEtNmU4ZGM0ZjkwMmZhXkEyXkFqcGc@._V1_.jpg"
+  },
+  {
+    title: "Spider-Man: Brand New Day",
+    genre: "Action",
+    showtime: "9:00 PM",
+    poster: "https://m.media-amazon.com/images/M/MV5BOWNjYWM3NWItOGE0ZS00MWRjLThiZWEtYjc4ZmNmMmU5ZTVmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+  },
+  {
+    title: "The Odyssey",
+    genre: "Adventure",
+    showtime: "9:30 PM",
+    poster: "https://upload.wikimedia.org/wikipedia/en/9/90/The_Odyssey_%282026_film%29_poster.jpg"
+  },
+  {
+    title: "Toy Story 5",
+    genre: "Animation",
+    showtime: "10:00 PM",
+    poster: "https://image.tmdb.org/t/p/original/pxG26JdyuiDvJbSoucknaFiLeZD.jpg"
+  },
+  {
+    title: "Motor City",
+    genre: "Thriller",
+    showtime: "10:30 PM",
+    poster: "https://image.tmdb.org/t/p/original/lGovcOtxugJPhfJjFKud9EbWtOA.jpg"
+  }
+];
 
 if (movies.length === 0) {
   movieList.innerHTML = "<p>No movies available.</p>";
@@ -19,18 +50,11 @@ if (movies.length === 0) {
 
     const button = card.querySelector("button");
     button.addEventListener("click", () => {
-      let bookings = JSON.parse(localStorage.getItem("bookings")) || [];
-      bookings.push({
-        title: movie.title,
-        genre: movie.genre,
-        showtime: movie.showtime,
-        poster: movie.poster
-      });
-      localStorage.setItem("bookings", JSON.stringify(bookings));
       alert(`${movie.title} has been booked!`);
     });
 
     movieList.appendChild(card);
   });
 }
+
 

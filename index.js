af779cfd3ccec4d4453ecb9e3f09e88e67cf25e2
Bobo635-hub <wanjohirachel7@gmail.com>
2026-08-06 06:59:05 -0700

    changed dollars to shillings

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const movies = [
     title: "Minions and Monsters",
     genre: "Comedy",
     showtime: "8:00 PM",
-    price: "$8",
+    price: "ksh 800",
     poster: "https://m.media-amazon.com/images/M/MV5BZjZkNzgyYTUtNjA1Yy00OTNmLTg5YWEtNmU4ZGM0ZjkwMmZhXkEyXkFqcGc@._V1_.jpg",
     trailer: "https://www.youtube.com/embed/HCYCke3VAZ0?si=JJFU1oIWCHCsp0Ry"
   },
@@ -13,7 +13,7 @@ const movies = [
     title: "Spider-Man: Brand New Day",
     genre: "Action",
     showtime: "9:00 PM",
-    price: "$12",
+    price: "ksh 1,200",
     poster: "https://m.media-amazon.com/images/M/MV5BOWNjYWM3NWItOGE0ZS00MWRjLThiZWEtYjc4ZmNmMmU5ZTVmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     trailer: "https://www.youtube.com/embed/7e9EH9gdgy0?si=qSIE8D-d4lN1Izho"
   },
@@ -21,7 +21,7 @@ const movies = [
     title: "The Odyssey",
     genre: "Adventure",
     showtime: "9:30 PM",
-    price: "$15",
+    price: "ksh 1,200",
     poster: "https://upload.wikimedia.org/wikipedia/en/9/90/The_Odyssey_%282026_film%29_poster.jpg",
     trailer: "https://www.youtube.com/embed/Mzw2ttJD2qQ?si=7kcwECh9IKopqojn"
   },
@@ -29,7 +29,7 @@ const movies = [
     title: "Toy Story 5",
     genre: "Animation",
     showtime: "10:00 PM",
-    price: "$10",
+    price: "ksh 1,000",
     poster: "https://image.tmdb.org/t/p/original/pxG26JdyuiDvJbSoucknaFiLeZD.jpg",
     trailer: "https://www.youtube.com/embed/c51ND9Hdbw0?si=qzLScVEFvNhKfU29" // placeholder
   },
@@ -37,7 +37,7 @@ const movies = [
     title: "Motor City",
     genre: "Thriller",
     showtime: "10:30 PM",
-    price: "$14",
+    price: "ksh 1,400",
     poster: "https://image.tmdb.org/t/p/original/lGovcOtxugJPhfJjFKud9EbWtOA.jpg",
     trailer: "https://www.youtube.com/embed/t6RklhKu9os?si=h5tlIXUYSrugd3f_"
   }
